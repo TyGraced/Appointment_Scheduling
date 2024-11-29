ccb91ef4da7779a91cb350d1133a2a624d618b3d
TyGraced <temitayoadesugba@gmail.com>
2024-11-30 00:47:35 +0100

    Exception Middleware, Logger, Service Extension

---
+++ Appointment_Scheduling/Appointment_Scheduling.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,100 @@
+﻿using Appointment_Scheduling.API.LoggerService;
+using Appointment_Scheduling.Core.Models;
+using Appointment_Scheduling.Core.Settings;
+using Appointment_Scheduling.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace Appointment_Scheduling.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(opt =>
+            {
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your token in the text input below.\n\nExample: `Bearer eyJhbGciOiJIUzI1NiIsInR..."
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                                        Scheme = "oauth2",
+                                        Name = "Bearer",
+                                        In = ParameterLocation.Header
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
+
+        public static void ConfigureNpgsqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<ApplicationDbContext>(opt =>
+                    opt.UseNpgsql(configuration.GetConnectionString("AppointmentSchedulingConnection")));
+
+        // Identity and Authentication
+        public static void AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration) =>
+                services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+
+        public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(opt =>
+            {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt =>
+            {
+                var jwtSettings = configuration.GetSection("JwtSettings").Get<JwtSettings>();
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings!.Issuer,
+                    ValidAudience = jwtSettings.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key!)),
+                };
+            });
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(opt =>
+            {
+                opt.SignIn.RequireConfirmedAccount = false;
+                opt.Password.RequireDigit = true;
+                opt.Password.RequiredLength = 8;
+                opt.Password.RequireNonAlphanumeric = true;
+                opt.Password.RequireUppercase = true;
+                opt.Password.RequireLowercase = true;
+            })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+                    services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
+}
---
+++ Appointment_Scheduling/Appointment_Scheduling.API/LoggerService/ILoggerManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Appointment_Scheduling.API.LoggerService
+{
+    public interface ILoggerManager
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+    }
+}
