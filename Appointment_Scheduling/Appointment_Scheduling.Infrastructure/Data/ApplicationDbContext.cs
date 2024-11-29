eb8477cf929a7cc89a3cc2a95381189dfb094f20
TyGraced <temitayoadesugba@gmail.com>
2024-11-29 09:23:05 +0100

    Added The Infratructure/Data Layer

---
+++ Appointment_Scheduling/Appointment_Scheduling.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Appointment_Scheduling.Core.Enums;
+using Appointment_Scheduling.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Appointment_Scheduling.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<ProviderAvailability> ProviderAvailability { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            var converter = new ValueConverter<AppointmentStatus, string>(
+                v => v.ToString(),
+                v => (AppointmentStatus)Enum.Parse(typeof(AppointmentStatus), v));
+
+            builder.Entity<Appointment>()
+                .Property(e => e.Status)
+                .HasConversion(converter);
+
+            builder.Entity<Appointment>()
+                .HasOne(a => a.Patient)
+                .WithMany()
+                .HasForeignKey(a => a.PatientId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+            builder.Entity<Appointment>()
+                .HasOne(a => a.Provider)
+                .WithMany()
+                .HasForeignKey(a => a.ProviderId)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+
+    }
+}
